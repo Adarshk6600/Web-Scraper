bd187278604d1bb919c22def60da1d7f18c38bd9
Adarshk6600 <adarshk6600@outlook.com>
2024-11-17 01:04:01 +0530

    push

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from 'next/image'
 import React from 'react'
 import Searchbar from '../components/Searchbar'
 import HeroCarousel from '../components/HeroCarousel'
+import { getAllProducts } from '../lib/actions'
+import ProductCards from '../components/ProductCards'
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts()
+
   return (
     <>
       <section className='px-6 md:px-20 py-24'>
@@ -28,10 +32,8 @@ const Home = () => {
       <section className='trending-section'>
         <h2 className='section-text'>Trending</h2>
         <div className='flex flex-wrap gap-x-8 gap-y-16'>
-          {['apple', 'iphone', 'book', 'sneakers'].map((product, index) => (
-            <div key={index}>
-              {product}
-            </div>
+          {allProducts?.map((product) => (
+            <ProductCards key={product._id} product={product} />
           ))}
          </div>
       </section>
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -40,3 +40,24 @@ export async function scrapeAndStoreProducts(productURL: string) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
 }
+
+export async function getProductById(productId:string) {
+  try {
+    dbConnect()
+    const product = await Product.findOne({_id:productId})
+    if (!product) return null;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAllProducts() {
+  try {
+    dbConnect()
+    const products = await Product.find()
+    return products
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
